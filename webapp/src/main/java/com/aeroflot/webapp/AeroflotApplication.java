e00834516cd4cef8ab74365bcbc3fb017687189e
am.moving.lightspeed <am.moving.lightspeed@gmail.com>
2021-06-09 18:08:09 +0300

    Initial commit (for the "Aeroflot" project with Spring Boot).

---
+++ webapp/src/main/java/com/aeroflot/webapp/AeroflotApplication.java
@@ -0,0 +1,13 @@
+package com.aeroflot.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroflotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroflotApplication.class, args);
+	}
+
+}
